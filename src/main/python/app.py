601c4b0fca4aeb86b89ef3afae79f319fde81e77
Peter Backeman <peter@backeman.se>
2020-06-05 11:39:53 +0200

    Fix drop-down menu to choose solvers

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,20 +1,26 @@
+# TODO:
+# - Add name for every node containing name of benchmark
+# - Check if we need data from outside CSV file?
+
+
+
+from collections import defaultdict
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 import pandas as pd
-import plotly.graph_objs as go
-from collections import defaultdict
-import plotly.express as px
-import os
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# df = pd.read_csv(
-#     'https://gist.githubusercontent.com/chriddyp/' +
-#     '5d1ea79569ed194d432e56108a04d188/raw/' +
-#     'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-#     'gdp-life-exp-2007.csv')
+# df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+# available_indicators = df['Indicator Name'].unique()
+
 
 # Fetch all benchmarks
 df = pd.read_csv('results.out')
@@ -22,53 +28,85 @@ df = pd.read_csv('results.out')
 benchmarks = df['benchmark'].drop_duplicates()
 
 results = defaultdict(dict)
-
+solvers = df['solver'].unique()
 for i, row in df.iterrows():
     bm = os.path.basename(row['benchmark'])
     solver = row['solver']
     time = row['time']
     results[bm][solver] = time
 
-
 x = []
 y = []
 
+data = defaultdict(dict)
+for s in solvers:
+    data[s] = []
 for k in results:
-    x.append(results[k]['z3_'])
-    y.append(results[k]['uppsat_SmallFloats'])
+    for s in solvers:
+        data[s].append(results[k][s])
 
+
+available_solvers = df['solver'].unique()
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='z3 vs SmallFloats',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=x,
-                    y=y,
-                    text='test',
-                    mode='markers',
-                    opacity=0.8,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) 
-            ],
-            'layout': go.Layout(
-                xaxis={'title': 'SmallFloats'},
-                yaxis={'title': 'z3'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_solvers],
+                value='SolverA'
             )
-        }
-    )
-])
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_solvers],
+                value='SolverB'
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+]
+)
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    [Input('xaxis-column', 'value'),
+     Input('yaxis-column', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name):
+    xx = data[xaxis_column_name]
+    yy = data[yaxis_column_name]
+    return {
+        'data': [dict(
+            x=xx,
+            y=yy,
+            text='Text',
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': dict(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' 
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' 
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
