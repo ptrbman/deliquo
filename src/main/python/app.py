100ffac73a9c4d5e88a45dd92849a177212ad18d
Peter Backeman <peter@backeman.se>
2020-06-07 11:15:29 +0200

    Add uploading of CSV file

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,41 +1,60 @@
 # TODO:
+# - Fix Select file to correctlt load data from CSV
 # - Check if we need data from outside CSV file?
-
-
 
 from collections import defaultdict
 import os
+import io
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-
+from dash.dependencies import Input, Output, State
 import pandas as pd
+import base64
+from dash.exceptions import PreventUpdate
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
-
-# available_indicators = df['Indicator Name'].unique()
-
-
 # Fetch all benchmarks
-df = pd.read_csv('results.out')
+# And put into easy to use structure
 
-benchmarks = []
-results = defaultdict(dict)
-solvers = df['solver'].unique()
-for i, row in df.iterrows():
-    bm = os.path.basename(row['benchmark'])
-    benchmarks.append(bm)
-    solver = row['solver']
-    time = row['time']
-    results[bm][solver] = time
+currentfile = ""
+solvers = "" # For now to make it global
+results = ""
+benchmarks = ""
+data = ""
+solverx = ""
+solvery = ""
 
-x = []
-y = []
+def parsecsv(df):
+    global solvers
+    global results
+    global benchmarks
+    global data
+    global solverx
+    global solvery
+    benchmarks = []
+    results = defaultdict(dict)
+    solvers = df['solver'].unique()
+
+    for i, row in df.iterrows():
+        bm = os.path.basename(row['benchmark'])
+        benchmarks.append(bm)
+        solver = row['solver']
+
+        time = row['time']
+        results[bm][solver] = time
+
+    data = defaultdict(dict)
+    for s in solvers:
+        data[s] = []
+    for k in results:
+        for s in solvers:
+            data[s].append(results[k][s])
+
+parsecsv(pd.read_csv('results.out'))
 
 def benchmarkstr(bm):
     fstr = bm + "\n"
@@ -43,25 +62,33 @@ def benchmarkstr(bm):
         fstr = fstr + s + ": " + str(results[bm][s]) + "\n"
     return fstr
 
-
-data = defaultdict(dict)
-for s in solvers:
-    data[s] = []
-for k in results:
-    for s in solvers:
-        data[s].append(results[k][s])
-
-
-available_solvers = df['solver'].unique()
-
 app.layout = html.Div([
     html.Div([
-
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '98%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=False
+    )]),
+    html.Div([
         html.Div([
             dcc.Dropdown(
                 id='xaxis-column',
-                options=[{'label': i, 'value': i} for i in available_solvers],
-                value=available_solvers[0]
+                options=[{'label': i, 'value': i} for i in solvers],
+                value=solvers[0]
             )
         ],
         style={'width': '48%', 'display': 'inline-block'}),
@@ -69,34 +96,26 @@ app.layout = html.Div([
         html.Div([
             dcc.Dropdown(
                 id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_solvers],
-                value=available_solvers[1]
+                options=[{'label': i, 'value': i} for i in solvers],
+                value=solvers[1]
             )
-        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+        ],style={'width': '48%', 'display': 'inline-block'}),
     ]),
 
     dcc.Graph(id='indicator-graphic'),
 
+    html.Div([
+        dcc.Markdown("Benchmark Data (click point)"),
+        html.Pre(id='click-data'),
+    ])
+])
 
-        html.Div([
-            dcc.Markdown("Benchmark Data (click point)"),
-            html.Pre(id='click-data'),
-        ], className='three columns')
 
-]
-)
-
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    [Input('xaxis-column', 'value'),
-     Input('yaxis-column', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name):
-    xx = data[xaxis_column_name]
-    yy = data[yaxis_column_name]
+def update_graph():
     return {
         'data': [dict(
-            x=xx,
-            y=yy,
+            x=data[solverx],
+            y=data[solvery],
             text=benchmarks,
             mode='markers',
             marker={
@@ -107,11 +126,11 @@ def update_graph(xaxis_column_name, yaxis_column_name):
         )],
         'layout': dict(
             xaxis={
-                'title': xaxis_column_name,
+                'title': solverx,
                 'type': 'linear' 
             },
             yaxis={
-                'title': yaxis_column_name,
+                'title': solvery,
                 'type': 'linear' 
             },
             margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
@@ -121,17 +140,40 @@ def update_graph(xaxis_column_name, yaxis_column_name):
 
 
 
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    [Input('xaxis-column', 'value'),
+     Input('yaxis-column', 'value'),
+     Input('upload-data', 'contents')],
+     [State('upload-data', 'filename')])
+def change_solvers(xaxis_column_name, yaxis_column_name, contents, filename):
+    global solverx
+    global solvery
+    print("change_solvers(", xaxis_column_name, ", ", yaxis_column_name, ", data, ", filename, ")")
+    solverx = xaxis_column_name
+    solvery = yaxis_column_name
+
+    if (filename != None):
+        try:
+            content_type, content_string = contents.split(',')
+            # Assume that the user uploaded a CSV file
+            decoded = base64.b64decode(content_string)
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            parsecsv(df)
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
+
+    return update_graph()
 
 @app.callback(
     Output('click-data', 'children'),
     [Input('indicator-graphic', 'clickData')])
 def display_click_data(clickData):
-    if (clickData == None):
-        print("Init")
-    else:
+    if (clickData != None):
         return benchmarkstr(clickData['points'][0]['text'])
-        
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
